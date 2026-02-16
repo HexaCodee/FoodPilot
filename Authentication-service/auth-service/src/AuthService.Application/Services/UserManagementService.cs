3a4e1ccd04e1b7bdec81b3618b6f1e7100ee1d63
dmolina-2024088 <dmolina-2024088@kinal.edu.gt>
2026-02-15 19:49:34 -0600

    fix: general error corrections across the project

--- Authentication-service/auth-service/src/AuthService.Application/Services/UserManagementService.cs
+++ Authentication-service/auth-service/src/AuthService.Application/Services/UserManagementService.cs
@@ -6,7 +6,7 @@ using AuthService.Domain.Interfaces;
 
 namespace AuthService.Application.Services;
 
-public class UserManagementService(IUserRepository users, IRoleRepository roles, ICloudinaryService cloudinary) : IUserManagementService
+public class UserManagementService(IUserRepository users, IRoleRepository roles) : IUserManagementService
 {
    
     public async Task<UserResponseDto> UpdateUserRoleAsync(string userId, string roleName)
@@ -53,7 +53,6 @@ public class UserManagementService(IUserRepository users, IRoleRepository roles,
             Surname = user.Surname,
             Username = user.Username,
             Email = user.Email,
-            ProfilePicture = cloudinary.GetFullImageUrl(user.UserProfile?.ProfilePicture ?? string.Empty),
             Phone = user.UserProfile?.Phone ?? string.Empty,
             Role = role.Name,
             Status = user.Status,
@@ -80,7 +79,6 @@ public class UserManagementService(IUserRepository users, IRoleRepository roles,
             Surname = u.Surname,
             Username = u.Username,
             Email = u.Email,
-            ProfilePicture = cloudinary.GetFullImageUrl(u.UserProfile?.ProfilePicture ?? string.Empty),
             Phone = u.UserProfile?.Phone ?? string.Empty,
             Role = roleName,
             Status = u.Status,
